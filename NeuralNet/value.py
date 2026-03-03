691904c05410743a8f3846d883b2f610b4bc174c
Distributed-Penguin <david.poss@gmail.com>
2026-03-03 11:32:21 +0200

    completed node-primitive

--- NeuralNet/value.py
+++ NeuralNet/value.py
@@ -3,18 +3,23 @@
 
 class Node:
 
-    def __init__ (self, num, _children = (), op = '', grad = 0):
+    def __init__ (self, num, _children = (), _op = ''):
         self.num = num
         self._prev = set(_children)
+        self._op = _op
     
     def __repr__ (self):
         return f""
     
     def __add__ (self, other):
-        return Node(self.num + other.num, (self, other))
+        return Node(self.num + other.num, (self, other), '+')
     
     def __mul__(self, other):
-        return Node(self.num + other.num, (self, other))
+        return Node(self.num + other.num, (self, other), '*')
 
+    def grad(self):
+        if self._children:
+            self.grad
+        
 if __name__ == "__main__":
     print("Hello World")
